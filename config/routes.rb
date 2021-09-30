df3b9109009559442711dd82bd32663e65d69d65
Jean Paul Fortuno <jp.fortuno@nullpod.com>
2021-09-30 21:48:04 +0400

    added: delete operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :gardens
+  resources :gardens do 
+    resources :plants, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :plants, only: :destroy
 end
